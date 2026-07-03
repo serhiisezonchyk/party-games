2945c8cc0da5f4d1a51fdd42ef2a0f6cb75a9cbb
serhii-halo <serhii.s@halo-lab.team>
2026-07-03 15:33:07 +0300

    fix: change top inset for telegram

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,7 @@ import {
 
 preventAutoHideAsync().catch(() => undefined);
 
-const TELEGRAM_FULLSCREEN_TOP_INSET = 78;
+const TELEGRAM_FULLSCREEN_TOP_INSET = 80;
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
